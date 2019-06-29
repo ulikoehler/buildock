0eece19077a17a66166733a18a24d47f04217729
Uli Köhler <ulikoehler@online.de>
2019-06-29 17:35:27 +0200

    Added buildock prototype function

---
+++ buildock.sh
@@ -0,0 +1,3 @@
+# buildock: https://github.com/ulikoehler/buildock
+# usage: buildock <image name> <command(s)>
+function buildock { IMAGE=$1 ; shift ; docker run --user $(id -u):$(id -g) -v "$(pwd):/app" -it ${IMAGE} $@ }
