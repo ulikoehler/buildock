19a3f3ee2534ac8f0ee5dafed273c25178b8c7cf
Uli Köhler <ulikoehler@online.de>
2019-06-29 18:38:45 +0200

    Use simpler shell argument processing

--- buildock.sh
+++ buildock.sh
@@ -5,7 +5,5 @@ function buildock {
     # No arguments warning
     if [[ $# -eq 0 ]] ; then echo "Usage: buildock <image name> <command(s)>" ; exit 1 ; fi
     # Actual buildock code
-    IMAGE=$1
-    shift
-    docker run --user $(id -u):$(id -g) -v "$(pwd):/app" -w "/app" --rm ${IMAGE} $@
+    docker run --user $(id -u):$(id -g) -v "$(pwd):/app" -w "/app" --rm $@
 }
