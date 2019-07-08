1fd5808567fd2a9af1d7cbdd70fb174bde91567f
Uli Köhler <ulikoehler@online.de>
2019-07-09 01:23:32 +0200

    Deprecate octave in favour of mtmiller/octave

--- ubuntu-octave-18-04/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export NAME=ulikoehler/ubuntu-octave
-export VERSION=18.04
-docker build -t ${NAME}:${VERSION} .
-docker push ${NAME}:${VERSION}
--- ubuntu-octave-19-04/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export NAME=ulikoehler/ubuntu-octave
-export VERSION=19.04
-docker build -t ${NAME}:${VERSION} .
-docker push ${NAME}:${VERSION}
