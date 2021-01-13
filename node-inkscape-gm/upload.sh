c090d00a8cda4ee1486c3090e1bb5ed2c8567484
Uli Köhler <ukoehler@techoverflow.net>
2021-01-13 17:33:49 +0100

    Update node-inkscape-gm

--- node-inkscape-gm/upload.sh
+++ node-inkscape-gm/upload.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export NAME=ulikoehler/node-inkscape-gm
-export VERSION=12
+export VERSION=14
 docker build -t ${NAME}:${VERSION} .
+docker build -t ${NAME}:lts .
 docker push ${NAME}:${VERSION}
+docker push ${NAME}:lts
