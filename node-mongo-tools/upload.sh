d2e240aa97f29983774c8cc2bc9b7d13e85e76cd
Uli Köhler <ukoehler@techoverflow.net>
2021-01-13 17:38:45 +0100

    Update node-mongo-tools

--- node-mongo-tools/upload.sh
+++ node-mongo-tools/upload.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export NAME=ulikoehler/node-mongo-tools
-export VERSION=12
+export VERSION=lts
 docker build -t ${NAME}:${VERSION} .
+docker build -t ${NAME}:14 .
 docker push ${NAME}:${VERSION}
+docker push ${NAME}:14
