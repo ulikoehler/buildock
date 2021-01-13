45fe28242b2c6aa2cface2a99d28fe178054deae
Uli Köhler <uli@techoverflow.net>
2021-01-13 03:17:34 +0100

    Fix buildock exiting the shell after printing usage

--- buildock.sh
+++ buildock.sh
@@ -3,7 +3,10 @@
 # usage: buildock [docker run argument(s)] <image name> <command(s)>
 function buildock {
     # No arguments warning
-    if [[ $# -eq 0 ]] ; then echo "Usage: buildock [docker run argument(s)] <image name> <command(s)>" ; exit 1 ; fi
-    # Actual buildock code
-    docker run -e HOME=/tmp --user $(id -u):$(id -g) -v "$(pwd):/app" -w "/app" --rm $@
+    if [[ $# -eq 0 ]] ; then
+        echo "Usage: buildock [docker run argument(s)] <image name> <command(s)>"
+    else
+        # Actual buildock code
+        docker run -e HOME=/tmp --user $(id -u):$(id -g) -v "$(pwd):/app" -w "/app" --rm $@ 
+    fi
 }
