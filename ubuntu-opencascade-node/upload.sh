8173732c74cda1ae4494eb945766f43f3989599b
Uli Köhler <ukoehler@techoverflow.net>
2021-09-28 23:28:44 +0200

    Add ubuntu-opencascade-node latest tag

--- ubuntu-opencascade-node/upload.sh
+++ ubuntu-opencascade-node/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export NAME=ulikoehler/ubuntu-opencascade-node
 export VERSION=16
-docker build -t ${NAME}:${VERSION} .
+docker build -t ${NAME}:${VERSION} -t ${NAME}:latest .
 docker push ${NAME}:${VERSION}
+docker push ${NAME}:latest
