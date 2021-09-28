964a50fc05420f9075603a952b989622faf5702e
Uli Köhler <ukoehler@techoverflow.net>
2021-09-28 16:13:37 +0200

    Fix zchunk docker version tags

--- zchunk/upload.sh
+++ zchunk/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export NAME=ulikoehler/zchunk
-export VERSION=1.1.2-fedora32
+export VERSION=1.1.9-fedora32
 docker build -t ${NAME}:${VERSION} -t ${NAME}:latest .
 docker push ${NAME}:${VERSION}
 docker push ${NAME}:latest
