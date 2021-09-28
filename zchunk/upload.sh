e4cb701d599805b280ccea66d1478779ace54b73
Uli Köhler <ukoehler@techoverflow.net>
2021-09-28 16:11:13 +0200

    Update zchunk to fedora 32

--- zchunk/upload.sh
+++ zchunk/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export NAME=ulikoehler/zchunk
-export VERSION=1.1.2-fedora30
-docker build -t ${NAME}:${VERSION} .
+export VERSION=1.1.2-fedora32
+docker build -t ${NAME}:${VERSION} -t ${NAME}:latest .
 docker push ${NAME}:${VERSION}
+docker push ${NAME}:latest
