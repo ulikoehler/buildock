b79df2dd6302d62b19f0762b6fc5f5f9e4159328
Uli Köhler <ukoehler@techoverflow.net>
2022-12-24 15:18:28 +0100

    Started to add bup build script

---
+++ bup/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf bup # Remove any old versions that might be present
+git clone -b 0.33 --depth 1 https://github.com/bup/bup
+export NAME=ulikoehler/bup
+export VERSION=0.33-alpine3.17
+export SHORTVERSION=0.33
+docker build -t ${NAME}:${SHORTVERSION} .
+docker build -t ${NAME}:${VERSION} .
+# docker push ${NAME}:${VERSION}
+# docker push ${NAME}:${SHORTVERSION}
