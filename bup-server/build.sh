d42c0e04899a9c7e97a337426eeb6cf9c0d69299
Uli Köhler <ukoehler@techoverflow.net>
2022-12-25 13:21:41 +0100

    Remove bup-remote to bup-server

--- bup-server/build.sh
+++ bup-server/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export NAME=ulikoehler/bup-remote
+export NAME=ulikoehler/bup-server
 export SHORTVERSION=0.33
 export VERSION=${SHORTVERSION}-alpine3.17
 docker build -t ${NAME}:${VERSION} -t ${NAME}:${SHORTVERSION} -t ${NAME}:latest . 
