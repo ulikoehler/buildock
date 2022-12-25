9804ed1c2d55e7a348465b3813ee1a8f93dbb8cf
Uli Köhler <ukoehler@techoverflow.net>
2022-12-25 08:51:58 +0100

    Make bup build & push work

--- bup/build.sh
+++ bup/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-rm -rf bup # Remove any old versions that might be present
+rm -rf bup par2cndline # Remove any old versions that might be present
 git clone -b 0.33 --depth 1 https://github.com/bup/bup
 export NAME=ulikoehler/bup
 export VERSION=0.33-alpine3.17
 export SHORTVERSION=0.33
-docker build -t ${NAME}:${SHORTVERSION} .
-#docker build -t ${NAME}:${VERSION} .
-# docker push ${NAME}:${VERSION}
-# docker push ${NAME}:${SHORTVERSION}
+docker build -t ${NAME}:${VERSION} .
+# Create additional tags
+docker image tag ${NAME}:${VERSION} ${NAME}:${SHORTVERSION}
+docker image tag ${NAME}:${VERSION} ${NAME}:latest
+# Push to repo
+docker push ${NAME}:${VERSION}
+docker push ${NAME}:${SHORTVERSION}
+docker push ${NAME}:latest
