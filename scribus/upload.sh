a9e28df0867c3ad3e1e4a0c3caa6b72946ed18f8
Uli Köhler <ukoehler@techoverflow.net>
2025-01-19 05:47:49 +0100

    Also build latest tag

--- scribus/upload.sh
+++ scribus/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export NAME=ulikoehler/scribus
 export VERSION=1.6.1 # Ubuntu 24.04
-docker build -t ${NAME}:${VERSION} .
+docker build -t ${NAME}:${VERSION} -t ${NAME}:latest .
 docker push ${NAME}:${VERSION}
